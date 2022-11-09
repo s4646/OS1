07891ba4861852d25148d2286d23e2bfe8fc8076
Sahar Tuvyahu <sahar45456@gmail.com>
2022-11-09 02:09:01 +0200

    Codec1 is complete

---
+++ codec1.c
@@ -0,0 +1,59 @@
+#include "codec.h"
+
+int encode(char* src, char* dst, int len)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+        if (isalpha(src[i]))
+        {
+            if (islower(src[i]))
+            {
+                dst[i] = toupper(src[i]);
+            }
+            else if (isupper(src[i]))
+            {
+                dst[i] = tolower(src[i]);
+            }
+            else
+            {
+                dst[i] = src[i];
+            }
+        }
+        else
+        {
+            dst[i] = src[i];
+        }
+    }
+    return 0;
+}
+
+int decode(char* src, char* dst, int len)
+{
+    return encode(src, dst, len);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        printf("Invalid input\n");
+        exit(-1);
+    }
+    const int length = strlen(argv[1]);
+    if (length < 0)
+    {
+        printf("Invalid input\n");
+        exit(-1);
+    }
+    
+    char encoded[length];
+    memset(encoded,'\0', length);
+    int check = encode(argv[1], encoded, length);
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("%c", encoded[i]);
+    }
+    printf("\n");
+    
+    return check;
+}
