00551f8543887ade6dcc614b79ea1c8eb90b525b
Sahar Tuvyahu <sahar45456@gmail.com>
2022-11-09 02:14:17 +0200

    Modified codec header file

--- codec.h
+++ codec.h
@@ -1,6 +1,11 @@
-
 #ifndef CODEC_H_INCLUDED
 #define CODEC_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 
 int encode(char* src, char* dst, int len);
 
