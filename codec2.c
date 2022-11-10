6d3daa0bf88bc53bc08f0d93d6cea86545e993b3
Sahar Tuvyahu <sahar45456@gmail.com>
2022-11-10 22:46:46 +0200

    Modified codec2.c

--- codec2.c
+++ codec2.c
@@ -2,14 +2,26 @@
 
 int encode(char* src, char* dst, int len)
 {
+    char x;
     for (size_t i = 0; i < len; i++)
     {
-            dst[i] = -src[i];
+        x = src[i];
+        x = ~x;
+        x = ((x & 1) << 1) ^ (x^1);
+        dst[i] = x;
     }
     return 0;
 }
 
 int decode(char* src, char* dst, int len)
 {
-    return encode(src, dst, len);
+    char x;
+    for (size_t i = 0; i < len; i++)
+    {
+        x = src[i];
+        x = x = x^1;;
+        x = ~x;
+        dst[i] = x;
+    }
+    return 0;
 }
