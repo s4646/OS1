c689b886f02f0b4e8172858a35d77c8e19fbe4b4
Sahar Tuvyahu <sahar45456@gmail.com>
2022-11-10 22:09:44 +0200

    copy.c now supports symlink copying

--- copy.c
+++ copy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,22 +9,79 @@
 int main(int argc, char** argv)
 {
     // input validation
-    if (argc != 3)
+    if (argc != 3 && argc !=4)
     {
-        printf("Invalid input\n");
+        printf("Usage: ./copy <file1> <file2>\n");
         exit(-1);
     }
-
+    
     // open files
-    int fd1 = open(argv[1], O_RDONLY);
-    int fd2 = open(argv[2], O_WRONLY);
-    if (fd1== -1 || fd2 == -1)
+    int fd1, fd2;
+    int opt = getopt(argc, argv, "l");
+    struct stat buf;
+    char linked[2048] = {'\0'};
+    if (opt != -1) // copy link contents
     {
-        printf("Error opening files\n");
+        if(readlink(argv[2], linked, 2048) == -1) // get symlink contents
+        {
+            perror("Error");
+            exit(-1);
+        }
+        fd2 = open(argv[3], O_WRONLY);
+        if (fd2 == -1)
+        {
+            perror("Error");
+            exit(-1);
+        }
+        if (write(fd2, linked, strlen(linked)) == -1)
+        {
+            perror("Error");
+            exit(-1);
+        }
+        else
+        {
+            printf("file is copied\n");
+            if (close(fd2) == -1)
+            {
+                perror("Error");
+                exit(-1);
+            } 
+        }
+        return 0;
+    }
+    else
+    {
+        if (lstat(argv[2], &buf) == -1) // get file's stats
+        {
+            perror("Error");
+            exit(-1);
+        }
+        if (S_ISLNK(buf.st_mode)) // if file is a symbolic link
+        {
+            if(readlink(argv[2], linked, 2048) == -1) // get symlink contents
+            {
+                perror("Error");
+                exit(-1);
+            }
+            fd1 = open(linked, O_RDONLY);
+            fd2 = open(argv[2], O_WRONLY);
+        }
+        else // file is original
+        {
+            fd1 = open(argv[1], O_RDONLY);
+            fd2 = open(argv[2], O_WRONLY);
+        }
+    }
+
+    if (fd1 == -1 || fd2 == -1)
+    {
+        perror("Error");
         exit(-1);
     }
     char c;
     size_t check1, check2;
+    
+    // copy files
     while(1)
     {
         check1 = read(fd1,&c,1);
@@ -31,7 +89,7 @@ int main(int argc, char** argv)
         {
             if (check1 == -1)
             {
-                printf("Error reading from file.\n");
+                perror("Error");
                 exit(-1);
             }
             if (check1 == 0)
@@ -42,15 +100,18 @@ int main(int argc, char** argv)
         check2 = write(fd2,&c,1);
         if (check2 == -1)
         {
-            printf("Error writing to file.\n");
+            perror("Error");
             exit(-1);
         }
         
     }
     
     printf("file is copied\n");
-    close(fd1);
-    close(fd2);
+    if (close(fd1) == -1 || close(fd2) == -1)
+    {
+        perror("Error");
+        exit(-1);
+    }
 
     return 0;
 }
